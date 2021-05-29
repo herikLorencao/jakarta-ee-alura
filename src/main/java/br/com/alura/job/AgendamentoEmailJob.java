b01881be88ddf8b1085875a4b8aa4ab4ea9b9b2c
Herik dos Santos Lorenção <heriksantoslorencao@gmail.com>
2021-05-29 16:49:46 -0300

    feat: transformando ejb em singleton
    Utilizando anotação Singleton para evitar problemas de concorrência e continuar tendo os benefícios do EJB

--- src/main/java/br/com/alura/job/AgendamentoEmailJob.java
+++ src/main/java/br/com/alura/job/AgendamentoEmailJob.java
@@ -4,15 +4,15 @@ import br.com.alura.entidade.AgendamentoEmail;
 import br.com.alura.servico.AgendamentoEmailServico;
 import java.util.List;
 import javax.ejb.Schedule;
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.inject.Inject;
 
-@Stateless
+@Singleton
 public class AgendamentoEmailJob {
 
     @Inject
     private AgendamentoEmailServico agendamentoEmailServico;
-    
+
     @Schedule(hour = "*", minute = "*", second = "*/10")
     public void enviarEmailsNaoAgendados() {
         List<AgendamentoEmail> emailsNaoAgendados = agendamentoEmailServico
